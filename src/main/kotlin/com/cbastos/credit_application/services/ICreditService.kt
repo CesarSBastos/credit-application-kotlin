86f16a9501e1af7ceeeb7512fa6124f8e645cb9e
CesarSBastos <cesarbastos32@gmail.com>
2025-02-25 23:30:30 -0300

    initial-service-layer-structure-customer-service

---
+++ src/main/kotlin/com/cbastos/credit_application/services/ICreditService.kt
@@ -0,0 +1,10 @@
+package com.cbastos.credit_application.services
+
+import com.cbastos.credit_application.entities.Credit
+import java.util.*
+
+interface ICreditService {
+    fun save(credit: Credit) : Credit
+    fun findAllByCustomer(customerId: Long) : List<Credit>
+    fun findByCreditCode(creditCode: UUID) : Credit
+}
